fd1ac63b4d946a5ebdea2e2d1f1f1c5ff77cea93
dingxin <dingxin@meituan.com>
2018-01-17 16:37:28 +0800

    rm

--- src/main/java/com/fateking/yi/controller/IndexController.java
+++ src/main/java/com/fateking/yi/controller/IndexController.java
@@ -30,37 +30,31 @@ public class IndexController {
 
     @GetMapping("/huobi/kline")
     public Result kline() {
-        GlobalContext.setToken("9r3Ixnx-IW4GnRwaeI_M-G9vwKGNl2uoZD5zEy5SVSgY-uOP2m0-gvjE57ad1qDF");
         return Result.success(SpringObjectFactory.getBean(MarketService.class).getKLine(Symbol.XRP_USDT, Period._1min, 200));
     }
 
     @GetMapping("/huobi/merged")
     public Result merged() {
-        GlobalContext.setToken("9r3Ixnx-IW4GnRwaeI_M-G9vwKGNl2uoZD5zEy5SVSgY-uOP2m0-gvjE57ad1qDF");
         return Result.success(SpringObjectFactory.getBean(MarketService.class).getMerged(Symbol.XRP_USDT));
     }
 
     @GetMapping("/huobi/trade")
     public Result trade() {
-        GlobalContext.setToken("9r3Ixnx-IW4GnRwaeI_M-G9vwKGNl2uoZD5zEy5SVSgY-uOP2m0-gvjE57ad1qDF");
         return Result.success(SpringObjectFactory.getBean(MarketService.class).getTrade(Symbol.XRP_USDT));
     }
 
     @GetMapping("/huobi/depth")
     public Result depth() {
-        GlobalContext.setToken("9r3Ixnx-IW4GnRwaeI_M-G9vwKGNl2uoZD5zEy5SVSgY-uOP2m0-gvjE57ad1qDF");
         return Result.success(SpringObjectFactory.getBean(MarketService.class).getDepth(Symbol.ETH_USDT, Type.Step1));
     }
 
     @GetMapping("/huobi/history/trade")
     public Result historyTrade() {
-        GlobalContext.setToken("9r3Ixnx-IW4GnRwaeI_M-G9vwKGNl2uoZD5zEy5SVSgY-uOP2m0-gvjE57ad1qDF");
         return Result.success(SpringObjectFactory.getBean(MarketService.class).getHistoryTrade(Symbol.XRP_USDT, 100));
     }
 
     @GetMapping("/huobi/detail")
     public Result detail() {
-        GlobalContext.setToken("9r3Ixnx-IW4GnRwaeI_M-G9vwKGNl2uoZD5zEy5SVSgY-uOP2m0-gvjE57ad1qDF");
         return Result.success(SpringObjectFactory.getBean(MarketService.class).getDetail(Symbol.XRP_USDT));
     }
 
